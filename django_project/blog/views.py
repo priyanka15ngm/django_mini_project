13c89b2a2a6932d49ca1c5070498189398cd9c34
unknown <priyanka.nigam_cs18@gla.ac.in>
2020-10-13 18:47:50 -0700

    updated

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+
+    },
+
+    {
+        'author': 'PriyankaER',
+        'title': 'Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+
+
+    }
+
+
+]
+
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
+
 
