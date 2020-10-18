591d8c5581128812aa8917fcd0ae7cca62f93ed9
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2020-10-18 22:41:47 +0530

    updated

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
 #django itself provide a user registration creation form
-from django.contrib.auth.forms import UserCreationForm
+#replacing UserCreationForm with UserRegisterForm
+
+#from django.contrib.auth.forms import UserCreationForm
 
 #IMPORTING MEASSAGES FROM DJANGO , THERE ARE SEVERAL MESSAGES LIKE messages.info, messages.success etc.
 from django.contrib import messages 
+
+#IMPORTING FORMS FROM forms.py
+
+from .forms import UserRegisterForm
 
 # Create your views here.
 #creating the visual look of users app and the we import this path to the main project
 def register(request):
      if request.method == 'POST':
-          form = UserCreationForm(request.POST)
+          #form = UserCreationForm(request.POST)
+          form = UserRegisterForm(request.POST)
           if form.is_valid():
                form.save()
                username =  form.cleaned_data.get('username')
@@ -17,7 +24,8 @@ def register(request):
                #after submitting return to the home page
                return redirect('blog-home')
      else:
-          form = UserCreationForm()
+          #form = UserCreationForm()
+          form = UserRegisterForm()
      return render(request, 'users/register.html', {'form':form})
 
 
