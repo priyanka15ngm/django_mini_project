d720a31b30a4a7ba880b93cb45a4456a5334850e
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2020-10-18 22:42:01 +0530

    created forms.py

---
+++ django_project/users/forms.py
@@ -0,0 +1,14 @@
+#importing forms
+from django import forms
+#importing models
+from django.contrib.auth.models import User
+#importing UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
